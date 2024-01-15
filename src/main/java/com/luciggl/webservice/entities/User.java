997f05394a9f5ce54ff347c8b0edd088cf83d4c8
Luciggl <lucielionascimento19@gmail.com>
2024-01-15 15:50:31 -0300

    feat:configuring database

--- src/main/java/com/luciggl/webservice/entities/User.java
+++ src/main/java/com/luciggl/webservice/entities/User.java
@@ -1,11 +1,19 @@
 package com.luciggl.webservice.entities;
 
+import jakarta.persistence.*;
+
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-    private static final long serialVERSIONUID = 1L;
+    @Serial
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ID;
     private String name;
     private String email;
