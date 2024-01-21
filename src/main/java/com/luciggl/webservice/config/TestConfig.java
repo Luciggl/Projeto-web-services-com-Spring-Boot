8c91d1bbfab68ff6701b7cce524e5a145578e36e
Luciggl <lucielionascimento19@gmail.com>
2024-01-20 21:55:50 -0300

    feat:Create OrderItem and implementation in dataBase

--- src/main/java/com/luciggl/webservice/config/TestConfig.java
+++ src/main/java/com/luciggl/webservice/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.luciggl.webservice.config;
 
-import com.luciggl.webservice.entities.Category;
-import com.luciggl.webservice.entities.Order;
-import com.luciggl.webservice.entities.Product;
-import com.luciggl.webservice.entities.User;
+import com.luciggl.webservice.entities.*;
 import com.luciggl.webservice.entities.enums.OrderStatus;
-import com.luciggl.webservice.repositories.CategoryRepository;
-import com.luciggl.webservice.repositories.OrderRepository;
-import com.luciggl.webservice.repositories.ProductRepository;
-import com.luciggl.webservice.repositories.UserRepository;
+import com.luciggl.webservice.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +27,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User("Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -54,6 +51,15 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+
+
+
         categoryRepository.saveAll(Arrays.asList(cat1, cat2,cat3));
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
@@ -65,5 +71,9 @@ public class TestConfig implements CommandLineRunner {
         p5.getCategories().add(cat2);
 
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+        orderItemRepository.saveAll(Arrays.asList(oi1,oi2, oi3,oi4));
+
+
+
     }
 }
---
+++ src/main/java/com/luciggl/webservice/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.luciggl.webservice.repositories;
+
+import com.luciggl.webservice.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
